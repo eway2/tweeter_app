0c330449cc92f97dc1829aa036d703d1bd869a76
Emma Way <eway2@lsu.edu>
2020-02-21 20:16:01 -0600

    email

--- tweeter_app/settings.py
+++ tweeter_app/settings.py
@@ -134,3 +134,10 @@ BOOTSTRAP4 = { 'include_jquery': True }
 
 LOGIN_REDIRECT_URL = 'home' 
 LOGOUT_REDIRECT_URL = 'home'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' # new
+EMAIL_HOST = 'smtp.sendgrid.net' # new
+EMAIL_HOST_USER = os.getenv('SENDGRID_USER')
+EMAIL_HOST_PASSWORD = os.getenv('SENDGRID_PASSWORD') # new
+EMAIL_PORT = 587 # new
+EMAIL_USE_TLS = True # new
